5db932b498a02dc5001835f2e8e35ba333709427
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-04-16 20:47:15 +0200

    "urls"

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
 
-class math(TestCase):
-	def test_bad_math(self):
-		self.assertEqual(1+1, 3)
+class user_vists_homepage(TestCase):
+	def test_user_hits_url(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
