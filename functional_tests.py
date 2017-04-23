1e2bf4eac391397b20b24a9d19fc1e400a9181b6
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-04-23 12:50:34 +0200

    Extending functional test and refactoring assertion code

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class user_visits_website(unittest.TestCase):
 	#special method that is started by the beginning of a test runner.	
@@ -23,10 +24,7 @@ class user_visits_website(unittest.TestCase):
 
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'		
-		)
+		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
@@ -35,12 +33,21 @@ class user_visits_website(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		
-		table = self.bowser.find_element_by_id('id_list_table')
+
+		time.sleep(1)
+
+		# She writes another to-do item and update the page. She sees all her input now.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Fartaka Fartaka 3al Tabala W3al Saksaka')
+		inputbox.send_keys(Keys.ENTER)
+
+		time.sleep(1)
+
+		# Page updated and she sees all her past input, a numerated list that contains all to-do items.
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Fartaka Fartaka 3al Tabala W3al Saksaka', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
--- lists/tests.py
+++ lists/tests.py
@@ -14,11 +14,28 @@ class user_vists_homepage(TestCase):
 	def test_home_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected = render_to_string('home.html')
+		#render_to_string takes a dictionary as a second parameter to map the variables values.
+		expected = render_to_string('home.html', request=request)
+
 		#decode converts bytes to python unicode string.
 		self.assertEqual(response.content.decode(), expected)
+
 		'''
 		--> we shouldn't test constants. We checked for behaviour instead. 
 		self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
 		self.assertIn(b'<title>To-Do Lists</title>', response.content)
 		self.assertTrue(response.content.endswith(b'</html>'))'''
+
+	def test_home_page_save_POST_request(self):
+		#Setup test
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		#Exercise test
+		response = home_page(request)
+
+		#Assert test
+		self.assertIn('A new list item', response.content.decode())
+
+	
